d86d15e61cc3aff08416fc636e25c50634c59b68
kholostyak17 <kholostyak17@gmail.com>
2021-07-16 00:58:25 +0000

    changed value true/false for sun/moon, and now is controlled by function

--- src/js/component/printvalue.js
+++ src/js/component/printvalue.js
@@ -1,22 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 const PrintValue = props => {
-	const [value, setValue] = useState(undefined);
-	let print = "";
-	if (value == true) {
-		print = "fas fa-sun";
-	}
-	if (value == false) {
-		print = "fas fa-moon";
-	}
+	// const [value, setValue] = useState(undefined);
+	const [print, setPrint] = useState("");
+	const myCallBackFunction = props.callbackfunc;
+
+	const setMyValue = () => {
+		if (props.value == "sun") {
+			setPrint(<i className="fas fa-sun"></i>);
+		}
+		if (props.value == "moon") {
+			setPrint(<i className="fas fa-moon"></i>);
+		}
+		myCallBackFunction(props.position);
+	};
+	console.log(props.value, "heyyyyyyyyyyyyyyyyy");
 	return (
-		<div className="value" onClick={() => setValue(props.value)}>
-			<i className={print}></i>
+		<div
+			className="col-4 square"
+			onClick={
+				print == ""
+					? setMyValue
+					: console.log("Esta casilla ya está pintada")
+			}>
+			{print}
 		</div>
 	);
 };
 export default PrintValue;
 
 PrintValue.propTypes = {
-	value: PropTypes.bool
+	value: PropTypes.string,
+	position: PropTypes.number,
+	callbackfunc: PropTypes.func
 };
+// const isEmptyValue = () => {
+// 	if (print != "") {
+// 		setValue(props.value);
+// 	}
+// };
