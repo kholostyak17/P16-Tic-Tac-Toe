82b8bf860854e36b0b1657f0bb7864447c58e145
kholostyak17 <kholostyak17@gmail.com>
2021-07-16 00:59:26 +0000

    board made with .map method and value is printed but still only sun

--- src/js/component/board.js
+++ src/js/component/board.js
@@ -1,85 +1,57 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import PropTypes, { element } from "prop-types";
 import PrintValue from "./printvalue.js";
 
 const Board = () => {
-	const [value, setValue] = useState(true);
+	const [value, setValue] = useState("sun");
+	const [myMap, setMyMap] = useState("");
+	const [myArray, setMyArray] = useState([
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	]);
 
-	function changeValue(position) {
-		setValue(!value);
-	}
+	const putValueInArray = position => {
+		console.log(myArray, "gato");
+		if (myArray[position] == "") {
+			console.log(value, "perro");
+			setValue(!value);
+			setMyArray(myArray.splice(position, 1, value));
+			if (value == "sun") {
+				console.log("si es sun ol");
+				setValue("moon");
+			}
+			if (value == "moon") {
+				setValue("sun");
+			}
+		}
+	};
+	useEffect(() => {
+		console.log(myArray);
+		setMyMap(
+			myArray.map((element, index) => {
+				console.log("HOLA!!!!");
+				return (
+					<PrintValue
+						key={index}
+						value={myArray[index]}
+						position={index}
+						callbackfunc={putValueInArray}
+					/>
+				);
+			})
+		);
+	}, []);
 
 	return (
 		<div className="container">
-			<div className="row">
-				<div
-					className="col-4 square"
-					onClick={() => {
-						changeValue(0);
-					}}>
-					<PrintValue value={value} />
-				</div>
-				<div
-					className="col-4 square"
-					onClick={() => {
-						changeValue(1);
-					}}>
-					<PrintValue value={value} />
-				</div>
-				<div
-					className="col-4 square"
-					onClick={() => {
-						changeValue(2);
-					}}>
-					<PrintValue value={value} />
-				</div>
-			</div>
-			<div className="row">
-				<div
-					className="col-4 square"
-					onClick={() => {
-						changeValue(3);
-					}}>
-					<PrintValue value={value} />
-				</div>
-				<div
-					className="col-4 square"
-					onClick={() => {
-						changeValue(4);
-					}}>
-					<PrintValue value={value} />
-				</div>
-				<div
-					className="col-4 square"
-					onClick={() => {
-						changeValue(5);
-					}}>
-					<PrintValue value={value} />
-				</div>
-			</div>
-			<div className="row">
-				<div
-					className="col-4 square"
-					onClick={() => {
-						changeValue(6);
-					}}>
-					<PrintValue value={value} />
-				</div>
-				<div
-					className="col-4 square"
-					onClick={() => {
-						changeValue(7);
-					}}>
-					<PrintValue value={value} />
-				</div>
-				<div
-					className="col-4 square"
-					onClick={() => {
-						changeValue(8);
-					}}>
-					<PrintValue value={value} />
-				</div>
-			</div>
+			<div className="row">{myMap}</div>
 		</div>
 	);
 };
