46efd17c7dae34fd956af65699f4c0b4da4af1c6
kholostyak17 <kholostyak17@gmail.com>
2021-07-17 19:13:57 +0000

    handleClick is saving the next step

--- src/js/component/game.js
+++ src/js/component/game.js
@@ -6,25 +6,36 @@ const Game = props => {
 	const [state, setState] = useState({
 		nextPlayer: true,
 		stepNumber: 0,
-		history: [["", "", "", "", "", "", "", "", ""]]
+		history: [{ squares: Array(9).fill(null) }]
 	});
 	const handleClick = position => {
 		console.log("click en casilla ", position + 1);
-		// 	const history = state.history.slice(0, state.stepNumber + 1);
-		// 	const currentPlayer = history[history.length - 1];  // selecciono el jugador actual
-		// 	const squares = currentPlayer.squares.slice();  //recoge array con todos los valores
-		// 	squares[index] = state.nextPlayer ? "X" : "O";  //asigno valor a casilla: true(sun) o false(moon)
-		// 	setState({
-		// 		nextPlayer: !state.nextPlayer,
-		// 		stepNumber: history.lenght,
-		// 		history: history.concat({squares: squares}),
-		// 	});  //alterno valor, asigno posición en array de la proxima acción,
+		const history = state.history.slice(0, state.stepNumber + 1);
+		const currentStep = history[history.length - 1]; // selecciono el jugador actual
+		const squares = currentStep.squares; //aqui hago una  array con todos los valores
+		squares[position] = state.nextPlayer ? true : false; //asigno valor a casilla: true(sun) o false(moon)
+		console.log(
+			history,
+			"pollas",
+			currentStep,
+			"squares",
+			squares,
+			squares[position]
+		);
+		setState({
+			nextPlayer: !state.nextPlayer,
+			stepNumber: history.lenght,
+			history: history.concat({ squares: squares })
+		}); //alterno valor, e introduzco el nuevo array en el historial
 	};
-	const currentValues = state.history[state.stepNumber];
-	console.log(currentValues, "hey");
+	const currentValuesToPrint = state.history[state.stepNumber];
+	console.log(currentValuesToPrint, "hey");
 	return (
 		<div>
-			<Board callBackFunc={handleClick} allSquares={currentValues} />
+			<Board
+				callBackFunc={handleClick}
+				allSquares={currentValuesToPrint}
+			/>
 		</div>
 	);
 };
